ece41bcc94018d016ee2193768fa7ed3c54ef20d
Дмитрий <78345276+DmitriySeredin@users.noreply.github.com>
2021-02-08 19:12:59 +0900

    add ProductsList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import TopHeader from "./Components/TopHeader/TopHeader";
 import Header from "./Components/Header/Header";
+import ProductsList from "./Components/ProductsList/ProductsList";
+
 const App = () => {
   return (
     <>
       <TopHeader />
       <Header />
+      <ProductsList />
     </>
   );
 };
