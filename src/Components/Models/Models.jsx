2afb789f4d9a24a46d9432df299949191f9ea6fe
Даниил Никитин <faverus061999@gmail.com>
2021-02-17 13:41:17 +0300

    Styles a bit

--- src/Components/Models/Models.jsx
+++ src/Components/Models/Models.jsx
@@ -2,30 +2,77 @@ import React from "react";
 import style from "./Models.module.css";
 const Models = () => {
   return (
-    <div className={style.models}>
-      <div className={style.wrapper}>
-        <img
-          src="https://content.asos-media.com/-/media/homepages/mw/2021/02/15/mw_artisan_outlook_moment_870x1110.jpg"
-          alt=""
-        />
-        <div className={style.block}>
-          <span>ДУХ ТВОРЧЕСТВА</span>
-          <p>Настроение творить</p>
-          <button>КУПИТЬ ТРЕНДОВЫЕ МОДЕЛИ</button>
-        </div>
-      </div>
-      <div className={style.wrapper}>
-        <img
-          src="https://content.asos-media.com/-/media/homepages/mw/2021/02/15/mw_bershka_moment_870x1110.jpg"
-          alt=""
-        />
-        <div className={style.block}>
-          <span>BERSHKA</span>
-          <p>Обнови свой стиль</p>
-          <button>КУПИТЬ</button>
-        </div>
-      </div>
-    </div>
+    <section className={style.section}>
+      <ul>
+        <li className={style.item}>
+          <div className={style.moment}>
+            <div className={style.momentWrap}>
+              <img
+                src="https://content.asos-media.com/-/media/homepages/mw/2021/02/15/mw_artisan_outlook_moment_870x1110.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className={style.text}>
+            <div className={style.textTitle}>
+              <h2>ДУХ ТВОРЧЕСТВА</h2>
+            </div>
+            <div className={style.textP}>
+              <p>Настроение творить</p>
+            </div>
+            <div className={style.btn}>
+              <span>КУПИТЬ ТРЕНДОВЫЕ МОДЕЛИ</span>
+            </div>
+          </div>
+        </li>
+        <li className={style.item}>
+          <div className={style.moment}>
+            <div className={style.momentWrap}>
+              <img
+                src="https://content.asos-media.com/-/media/homepages/mw/2021/02/15/mw_bershka_moment_870x1110.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className={style.text}>
+            <div className={style.textTitle}>
+              <h2>BERSHKA</h2>
+            </div>
+            <div className={style.textP}>
+              <p>Обнови свой стиль</p>
+            </div>
+            <div className={style.btn}>
+              <span>КУПИТЬ</span>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
   );
 };
 export default Models;
+
+// <div className={style.models}>
+//   <div className={style.wrapper}>
+//     <img
+//       src="https://content.asos-media.com/-/media/homepages/mw/2021/02/15/mw_artisan_outlook_moment_870x1110.jpg"
+//       alt=""
+//     />
+//     <div className={style.block}>
+//       <span>ДУХ ТВОРЧЕСТВА</span>
+//       <p>Настроение творить</p>
+//       <button>КУПИТЬ ТРЕНДОВЫЕ МОДЕЛИ</button>
+//     </div>
+//   </div>
+//   <div className={style.wrapper}>
+//     <img
+//       src="https://content.asos-media.com/-/media/homepages/mw/2021/02/15/mw_bershka_moment_870x1110.jpg"
+//       alt=""
+//     />
+//     <div className={style.block}>
+//       <span>BERSHKA</span>
+//       <p>Обнови свой стиль</p>
+//       <button>КУПИТЬ</button>
+//     </div>
+//   </div>
+// </div>
