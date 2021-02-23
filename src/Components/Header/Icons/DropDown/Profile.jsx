a2a12e050a1c4256fdcc15a93e62434e053ff2bd
Даниил Никитин <faverus061999@gmail.com>
2021-02-23 12:28:12 +0300

    Add profile button + component

---
+++ src/Components/Header/Icons/DropDown/Profile.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import style from "./Profile.module.css";
+
+const Profile = () => {
+  return (
+    <div className={style.profile}>
+      <div className={style.entry}>
+        <span>Войти</span>
+        <span>Регистрация</span>
+      </div>
+      <div className={style.options}>
+        <ul>
+          <li>
+            <img
+              src="https://pics.freeicons.io/uploads/icons/png/19339625881548233621-512.png"
+              alt=""
+            />
+            <p>Личный кабинет</p>
+          </li>
+          <li>
+            <img
+              src="https://img.icons8.com/ios/452/shopping-cart.png"
+              alt=""
+            />
+            <p>Мои заказы</p>
+          </li>
+          <li>
+            <img src="https://img.icons8.com/pastel-glyph/2x/info.png" alt="" />
+            <p>Информация о возвратах</p>
+          </li>
+          <li>
+            <img
+              src="https://images.vexels.com/media/users/3/136808/isolated/preview/d3455a22af5f3ed7565fb5fb71bb8d43-send-message-icon-by-vexels.png"
+              alt=""
+            />
+            <p>Предпочтительные способы связи</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- src/Components/Header/Icons/Icons.jsx
+++ src/Components/Header/Icons/Icons.jsx
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Profile from "./DropDown/Profile";
 import style from "./Icons.module.css";
 const Icons = () => {
+  const [value, setValue] = useState(false);
+  let toggle = (value) => {
+    if (value === false) {
+      return true;
+    }
+    return false;
+  };
   return (
     <div className={style.icons}>
       <div className={style.border}>
-        <img
-          className={style.img}
-          src="https://pics.freeicons.io/uploads/icons/png/19339625881548233621-512.png"
-          alt="Профиль"
-        />
+        <button onClick={() => setValue(toggle)}>
+          <img
+            className={style.img}
+            src="https://pics.freeicons.io/uploads/icons/png/19339625881548233621-512.png"
+            alt="Профиль"
+          />
+        </button>
+        {value ? <Profile /> : null}
       </div>
       <div className={style.border}>
-        <img
-          src="https://image.flaticon.com/icons/png/24/9/9294.png"
-          alt=""
-          className={style.img}
-        />
+        <button>
+          <img
+            src="https://image.flaticon.com/icons/png/24/9/9294.png"
+            alt=""
+            className={style.img}
+          />
+        </button>
       </div>
       <div className={style.border}>
-        <img
-          src="https://cdn2.iconfinder.com/data/icons/celebration-party/48/88-512.png"
-          alt=""
-          className={style.img}
-        />
+        <button>
+          <img
+            src="https://cdn2.iconfinder.com/data/icons/celebration-party/48/88-512.png"
+            alt=""
+            className={style.img}
+          />
+        </button>
       </div>
     </div>
   );
